b9424c608aa896998d42afbbc37b326c1db088d8
Sean Buttinger <sean@antfarm.de>
2024-12-02 15:12:49 +0100

    Day <n> template

---
+++ AdventOfCode2024/DayN.swift
@@ -0,0 +1,28 @@
+//
+//  DayN.swift
+//  AdventOfCode2024
+//
+//  Created by Sean Buttinger on 02.12.24.
+//
+
+import Foundation
+
+
+struct DayN {
+    
+    static func part1(_ input: String) -> String {
+        
+        let lines = input.components(separatedBy: "\n")
+        
+        return String(lines.count)
+    }
+    
+    
+    static func part2(_ input: String) -> String {
+        
+        let lines = input.components(separatedBy: "\n")
+        
+        return String(lines.count)
+    }
+}
+
