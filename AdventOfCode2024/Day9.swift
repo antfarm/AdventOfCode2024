92349540b9d61440ce8139c586a5f0bb3ce831b9
Sean Buttinger <sean@antfarm.de>
2024-12-09 23:49:01 +0100

    Day 9 part 1

---
+++ AdventOfCode2024/Day9.swift
@@ -0,0 +1,67 @@
+//
+//  Day9.swift
+//  AdventOfCode2024
+//
+//  Created by Sean Buttinger on 09.12.24.
+//
+
+import Foundation
+
+
+struct Day9 {
+    
+    static func part1(_ input: String) -> String {
+
+        let disk = blocks(fromDiskMap: input)
+        let defragmentedDisk = defragmentMutable(disk: disk)
+        
+        let checksum = checksum(disk: defragmentedDisk)
+        
+        return String(checksum)
+    }
+    
+
+    fileprivate static func defragment(disk: [Int]) -> [Int] {
+        
+        guard let index = disk.firstIndex(of: -1) else {
+            return disk
+        }
+        
+        return defragment(disk: Array(disk[0..<index] + [disk.last!] + disk[index+1..<disk.count-1]))
+    }
+    
+    
+    fileprivate static func defragmentMutable(disk: [Int]) -> [Int] {
+        
+        var mutableDisk = disk
+        
+        while true {            
+            guard let index = mutableDisk.firstIndex(of: -1) else {
+                return mutableDisk
+            }
+            
+            mutableDisk = Array(mutableDisk[0..<index] + [mutableDisk.last!] + mutableDisk[index+1..<mutableDisk.count-1])
+        }
+    }
+
+    
+    fileprivate static func checksum(disk: [Int]) -> Int {
+        
+        disk.enumerated().reduce(0) { sum, element in
+            let (index, id) = element
+            return sum + (index * id)
+        }
+    }
+    
+    
+    fileprivate static func blocks(fromDiskMap input: String) -> [Int] {
+        
+        let numbers = Array(input).map { Int(String($0))! }
+        
+        return numbers.enumerated().reduce([Int]()) { blocks, element in
+            let (index, number) = element
+            
+            return blocks + Array(repeating: index % 2 == 0 ? (index / 2) : -1, count: number)
+        }
+    }
+}
